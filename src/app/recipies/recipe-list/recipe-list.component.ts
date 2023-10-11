5eac4df719e09e5270f3390d9be04058772113a6
Shakeeb <shakeebparavannur@gmail.com>
2023-10-11 12:43:21 +0530

    first commit

---
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Test Recipe',
+      'This is test description',
+      'https://images.pexels.com/photos/1640771/pexels-photo-1640771.jpeg?auto=compress&cs=tinysrgb&w=600'
+    ),
+  ];
+}
